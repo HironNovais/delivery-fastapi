1af438b2570571acf9bdab12051e5d5866ec48eb
HironNovais <hiron_novais@hotmail.com>
2026-02-05 23:07:41 -0300

    Aula 04 - HashTagProgramação (Criação de Session, Consulta de Usuário, Schemas, Criptografia[bcrypt], HttpException, Depends)

--- auth_routes.py
+++ auth_routes.py
@@ -1,10 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from models import Usuario
+from dependencies import pegar_sessao
+from main import bcrypt_context
+from schemas import UsuarioSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get("/")
-async def autenticar():
+async def home():
     """
     Rota de autenticação
     """
     return {"mensagem": "Rota de autenticação", "status": False}
+
+@auth_router.post("/criar_conta")
+async def criar_conta(usuario_schema: UsuarioSchema, session=Depends(pegar_sessao)):
+
+    usuario = session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
+    
+    if usuario:
+        raise HTTPException(status_code=400, detail="E-mail já cadastrado")
+    else:
+        senha_criptografada = bcrypt_context.hash(usuario_schema.senha)
+        novo_usuario = Usuario(nome=usuario_schema.nome, email=usuario_schema.email, senha=senha_criptografada, ativo=usuario_schema.ativo, admin=usuario_schema.admin)
+        session.add(novo_usuario)
+        session.commit()
+        return {"mensagem": "Conta criada com sucesso", "status": True}
